09b35b157b82915c371e4847439429b1bc898283
Alex Snigur <aleksei.snigur@mail.ee>
2019-10-15 11:40:26 +0300

    #p14-15 Alex Snigur, work with power and health intervals

--- src/org/btarikool/javacourse/Game.java
+++ src/org/btarikool/javacourse/Game.java
@@ -5,8 +5,26 @@ public class Game {
 
     public static void main(String[] args) {
         System.out.println("LORDS COUNT: " + Settings.getLordsCount());
-        System.out.println("LORDS INTERVAL FROM: " + Settings.getLordsHealthInterval()[0]);
-        System.out.println("LORDS INTERVAL TO: " + Settings.getLordsHealthInterval()[1]);
+        System.out.println("LORDS HEALTH INTERVAL FROM: " + Settings.getLordsHealthInterval()[0]);
+        System.out.println("LORDS HEALTH INTERVAL TO: " + Settings.getLordsHealthInterval()[1]);
+        System.out.println("LORDS POWER INTERVAL FROM: "+ Settings.getLordsPowerInterval()[0]);
+        System.out.println("LORDS POWER INTERVAL TO:" + Settings.getLordsPowerInterval()[1]);
+        System.out.println("KNIGHTS COUNT: " + Settings.getKnightsCount());
+        System.out.println("KNIGHTS HEALTH INTERVAL FROM: " + Settings.getKnightsHealthInterval()[0]);
+        System.out.println("KNIGHTS HEALTH INTERVAL TO: " + Settings.getKnightsHealthInterval()[1]);
+        System.out.println("KNIGHTS POWER INTERVAL FROM: " + Settings.getKnightsPowerInterval()[0]);
+        System.out.println("KNIGHTS POWER INTERVAL TO: " + Settings.getKnightsPowerInterval()[1]);
+        System.out.println("WIZARDS COUNT: " + Settings.getWizardsCount());
+        System.out.println("WIZARDS HEALTH INTERVAL FROM " + Settings.getWizardsHealthInterval()[0]);
+        System.out.println("WIZARDS HEALTH INTERVAL TO " + Settings.getWizardsHealthInterval()[1]);
+        System.out.println("WIZARDS POWER INTERVAL FROM: " + Settings.getWizardsPowerInterval()[0]);
+        System.out.println("WIZARDS POWER INTERVAL TO " + Settings.getWizardsPowerInterval()[1]);
+        System.out.println("PEASANTS COUNT " + Settings.getPeasantsCount());
+        System.out.println("PEASANTS HEALTH INTERVAL FROM " + Settings.getPeasantsHealthInterval()[0]);
+        System.out.println("PEASANTS HEALTH INTERVAL TO"  + Settings.getPeasantsHealthInterval()[1]);
+        System.out.println("PEASANTS POWER INTERVAL FROM " + Settings.getPeasantsPowerInterval()[0]);
+        System.out.println("PEASANTS POWER INTERVAL TO " + Settings.getPeasantsPowerInterval()[1]);
+
         Kingdom kingdom1 = new Kingdom("FarAway");
         King richard = (King) kingdom1.createPerson("Richard", "King", null);
         Lord lionel = (Lord) kingdom1.createPerson("Lionel", "Lord", richard);
--- src/org/btarikool/javacourse/Settings.java
+++ src/org/btarikool/javacourse/Settings.java
@@ -10,9 +10,9 @@ public class Settings {
 
     static {
         properties = new Properties();
-        try(InputStream input = new FileInputStream("conf/default.properties")) {
+        try (InputStream input = new FileInputStream("conf/default.properties")) {
             properties.load(input);
-        } catch(IOException ioe) {
+        } catch (IOException ioe) {
             System.out.println("properties file not found");
         }
 
@@ -24,19 +24,7 @@ public class Settings {
         return count;
     }
 
-    public static int getKnigtsCount() {
-        String countStr = properties.getProperty("knights.count");
-        int count = Integer.parseInt(countStr);
-        return count;
-    }
-
-    public static int getWizardsCount() {
-        String countStr = properties.getProperty("wizards.count");
-        int count = Integer.parseInt(countStr);
-        return count;
-    }
-
-    public static int[] getLordsPowerInterval () {
+    public static int[] getLordsPowerInterval() {
         String interval = properties.getProperty("lord.power.interval");
         String[] intervalArr = interval.split("-"); // 7-9 => {"7", "9"}
         int[] ret = new int[2]; //  {0, 0}
@@ -45,7 +33,7 @@ public class Settings {
         return ret;
     }
 
-    public static double[] getLordsHealthInterval () {
+    public static double[] getLordsHealthInterval() {
         String interval = properties.getProperty("lord.health.interval");
         String[] intervalArr = interval.split("-"); // 0.7-0.9 => {"0.7", "0.9"}
         double[] ret = new double[2]; //  {0.0, 0.0}
@@ -54,7 +42,75 @@ public class Settings {
         return ret;
     }
 
+    public static int getKnightsCount() {
+        String countStr = properties.getProperty("knights.count");
+        int count = Integer.parseInt(countStr);
+        return count;
+    }
 
+    public static int[] getKnightsPowerInterval() {
+        String interval = properties.getProperty("knight.power.interval");
+        String[] intervalArr = interval.split("-"); // 5-9 => {"5", "9"}
+        int[] ret = new int[2]; //  {0, 0}
+        ret[0] = Integer.parseInt(intervalArr[0]);// {2, 5}
+        ret[1] = Integer.parseInt(intervalArr[1]); // {7, 9}
+        return ret;
+    }
 
+    public static double[] getKnightsHealthInterval() {
+        String interval = properties.getProperty("knight.health.interval");
+        String[] intervalArr = interval.split("-"); // 0.4-0.9 => {"0.4", "0.9"}
+        double[] ret = new double[2]; //  {0.0, 0.0}
+        ret[0] = Double.valueOf(intervalArr[0]);// {0.4, 0.0}
+        ret[1] = Double.valueOf(intervalArr[1]); // {0.4, 0.9}
+        return ret;
+    }
 
+    public static int getWizardsCount() {
+        String countStr = properties.getProperty("wizards.count");
+        int count = Integer.parseInt(countStr);
+        return count;
+    }
+
+    public static int[] getWizardsPowerInterval() {
+        String interval = properties.getProperty("wizard.power.interval");
+        String[] intervalArr = interval.split("-"); // 9-12 => {"9", "12"}
+        int[] ret = new int[2]; //  {0, 0}
+        ret[0] = Integer.parseInt(intervalArr[0]);// {9, 0}
+        ret[1] = Integer.parseInt(intervalArr[1]); // {9, 12}
+        return ret;
+    }
+
+    public static double[] getWizardsHealthInterval() {
+        String interval = properties.getProperty("wizard.health.interval");
+        String[] intervalArr = interval.split("-"); // 0.9-1.0 => {"0.9", "1.0"}
+        double[] ret = new double[2]; //  {0.0, 0.0}
+        ret[0] = Double.valueOf(intervalArr[0]);// {0.9, 0.0}
+        ret[1] = Double.valueOf(intervalArr[1]); // {0.9, 1.0}
+        return ret;
+    }
+
+    public static int getPeasantsCount() {
+        String countStr = properties.getProperty("peasants.count");
+        int count = Integer.parseInt(countStr);
+        return count;
+    }
+
+    public static int[] getPeasantsPowerInterval() {
+        String interval = properties.getProperty("peasant.power.interval");
+        String[] intervalArr = interval.split("-"); // 3-5 => {"3", "5"}
+        int[] ret = new int[2]; //  {0, 0}
+        ret[0] = Integer.parseInt(intervalArr[0]);// {3, 0}
+        ret[1] = Integer.parseInt(intervalArr[1]); // {3, 5}
+        return ret;
+    }
+
+    public static double[] getPeasantsHealthInterval() {
+        String interval = properties.getProperty("peasant.health.interval");
+        String[] intervalArr = interval.split("-"); // 0.8-0.9 => {"0.8", "0.9"}
+        double[] ret = new double[2]; //  {0.0, 0.0}
+        ret[0] = Double.valueOf(intervalArr[0]);// {0.8, 0.0}
+        ret[1] = Double.valueOf(intervalArr[1]); // {0.8, 0.9}
+        return ret;
+    }
 }
